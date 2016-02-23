ed16b5cca3e384670e8a8e07c36bc2e5f9fc4316
fafcrumb <bri.ferch@gmail.com>
2016-02-23 18:16:22 -0500

    Fixed car rendering position

--- View.cpp
+++ View.cpp
@@ -10,17 +10,20 @@ View::View(Model* m)
 	window.create(sf::VideoMode(225, 225), "Assignment 3");
 	window.setFramerateLimit(60);
 
+	//Map
 	map.load("Assets/tileset.jpg", sf::Vector2u(32, 32), model->cameraTiles, model->cameraWidth, model->cameraHeight);
 
+	//Player
 	player.setSize(sf::Vector2f(32, 32));
 	player.setOrigin(sf::Vector2f(16, 16));
 	player.setFillColor(sf::Color::Red);
 	player.setPosition(sf::Vector2f(model->playerX, model->playerY));
 
-	car.setSize(sf::Vector2f(32, 10));
-	car.setOrigin(sf::Vector2f(16, 5));
+	//Car
+	car.setSize(sf::Vector2f(22, 10));
+	car.setOrigin(sf::Vector2f(11, 5));
 	car.setFillColor(sf::Color::Blue);
-	car.setPosition(model->carLocation);
+	car.setPosition(sf::Vector2f(model->carLocation.x - model->cameraX, model->carLocation.y - model->cameraY));
 }
 
 View::~View()
@@ -30,20 +33,25 @@ void View::render()
 {
 	window.clear();
 
-	//map.setPosition(sf::Vector2f(-model->cameraX, -model->cameraY));
+	//Map
 	map.setPosition(sf::Vector2f(-(int)model->cameraX % 32, -(int)model->cameraY % 32));
 	map.load("Assets/tileset.jpg", sf::Vector2u(32, 32), model->cameraTiles, model->cameraWidth, model->cameraHeight);
 	window.draw(map);
 
+	//Player
 	player.setPosition(sf::Vector2f(model->playerX - model->cameraX, model->playerY - model->cameraY));
 	window.draw(player);
 
+	//Car
+	float PI = 3.14159265;
 	car.setPosition(sf::Vector2f(model->carLocation.x - model->cameraX, model->carLocation.y - model->cameraY));
-	car.setRotation(model->carHeading * 90);
+	car.setRotation(model->carHeading * 180 / PI);
 	window.draw(car);
 
 	window.display();
 
+
+	//Optional console based grid view (set framerate to 2 or 3 when using)
 	/*for (int i = 0; i < model->mapHeight; i++)
 	{
 		for (int j = 0; j < model->mapWidth; j++)
