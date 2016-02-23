457e871111849f613b113f920acd2bc71d7da406
fafcrumb <bri.ferch@gmail.com>
2016-02-23 18:06:53 -0500

    Update Model.hpp
    Comments

--- Model.hpp
+++ Model.hpp
@@ -12,23 +12,27 @@ public:
 
 	void update(sf::Time);
 
+	//Map
 	int mapWidth, mapHeight;
 	int **mapTiles;
 
+	//Camera
 	int cameraWidth, cameraHeight;
 	int cameraCol, cameraRow;
 	float cameraX, cameraY;
 	int *cameraTiles;
 
+	//Player
 	int playerCol, playerRow;
 	float playerX, playerY;
 	int playerSpeed;
 
+	//Car
 	int carCol, carRow;
 
 	sf::Vector2f carLocation;
 	float carHeading;
 	float carSpeed;
 	float steerAngle;
-	float wheelBase; // the distance between the two axles	
+	float wheelBase; // the distance between the two axles
 };
