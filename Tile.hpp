a34f5a011e426fe8fa09ce424dd280c8ad981ca5
BenVarao <benvarao@hotmail.com>
2016-03-09 03:34:25 -0500

    Create Tile.hpp

---
+++ Tile.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+// This class is used to hold the data for the individual tiles on the map neccesary for the interaction between the player vehicle and the land scape
+// This class is used by classes that include model with the Tilemap getting the neccesary information from this class through camera
+class Tile 
+{
+public:
+	Tile(int);
+	~Tile();
+	int returnBlockable();
+	int returnMapCode();
+
+private:
+	int blockability;
+	int activeMapCode;
+	int baseMapCode;
+};
