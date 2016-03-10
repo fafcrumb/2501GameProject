8167797e70846f6627d7d0cad0896e3a8db01f2f
Jyrax <tristan@phillipses.net>
2016-03-09 19:34:36 -0500

    Update Speedometer.hpp

--- Speedometer.hpp
+++ Speedometer.hpp
@@ -1,1 +1,27 @@
 #pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include "Model.hpp"
+#include "View.hpp"
+
+
+class Speedometer : public Updateable, public Renderable
+{
+public:
+	Speedometer();
+	~Speedometer();
+	void update(Model*, sf::Time deltaTime);
+	void render(View*);
+private:
+	float angle;
+	float x, y;
+
+	sf::Image baseImage;
+	sf::Texture baseTexture;
+	sf::Sprite baseSprite;
+
+	sf::Image needleImage;
+	sf::Texture needleTexture;
+	sf::Sprite needleSprite;
+};
