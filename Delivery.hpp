811e2d4c23eb0c0d5974798fdde54d5a1221a0df
Jyrax <tristan@phillipses.net>
2016-03-23 22:18:41 -0400

    Update Delivery.hpp

--- Delivery.hpp
+++ Delivery.hpp
@@ -10,17 +10,22 @@ public:
 	bool checkExpired(sf::Time);
 	sf::Vector2u getPickupPoint() { return pickupPoint; }
 	sf::Vector2u getDropoffPoint() { return dropoffPoint; }
+	sf::Color getColor() { return color; }
 	std::string getImageFile() { return imageFile; }
 	void pickUp() { pickedUp = true; }
 	bool isPickedUp() { return pickedUp; }
 	void dropOff() { droppedOff = true; }
 	bool isDroppedOff() { return droppedOff; }
+	void miniChange() { miniMe = !miniMe; }
+	bool getMiniMe() { return miniMe; }
 private:
 	std::string itemName;
 	std::string imageFile;
 	sf::Vector2u pickupPoint;
 	sf::Vector2u dropoffPoint;
 	sf::Time timeLimit;
+	sf::Color color;
 	bool pickedUp;
 	bool droppedOff;
+	bool miniMe;
 };
