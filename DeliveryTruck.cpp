bfc8a3ecbb79e1f627753c9caede6996e6d0f30f
BenVarao <benvarao@hotmail.com>
2016-03-23 22:56:45 -0400

    Update DeliveryTruck.cpp

--- DeliveryTruck.cpp
+++ DeliveryTruck.cpp
@@ -1,5 +1,6 @@
 #include "DeliveryTruck.hpp"
 
+
 DeliveryTruck::DeliveryTruck()
 {
 	col = 6;
@@ -15,6 +16,9 @@ DeliveryTruck::DeliveryTruck()
 	car.setTexture(texture);
 	car.setOrigin(texture.getSize().x / 2, texture.getSize().y / 2);
 	car.setRotation(heading * 180 / PI);
+	moveTest.setSize(sf::Vector2f(71, 131));
+	moveTest.setOrigin(sf::Vector2f(71 / 2.f, 131 / 2.f));
+	moveTest.setPosition(sf::Vector2f(location.x, location.y));
 }
 
 DeliveryTruck::~DeliveryTruck()
@@ -23,12 +27,12 @@ DeliveryTruck::~DeliveryTruck()
 void DeliveryTruck::update(Model* model, sf::Time deltaTime)
 {
 	//Max speed
-	if (abs(speed) > maxSpeed)
+	if (abs(speed) > 600)
 	{
 		if (speed < 0)
 			speed = -maxSpeed;
 		else
-			speed = maxSpeed;
+			speed = MaxSpeed;
 	}
 
 	//Friction
@@ -53,9 +57,31 @@ void DeliveryTruck::update(Model* model, sf::Time deltaTime)
 
 	backWheel += speed * deltaTime.asSeconds() * sf::Vector2f(cos(heading), sin(heading));
 	frontWheel += speed * deltaTime.asSeconds() * sf::Vector2f(cos(heading + steerAngle), sin(heading + steerAngle));
+	moveTest.setPosition((frontWheel + backWheel) / 2.f);
+	moveTest.setRotation(atan2(frontWheel.y - backWheel.y, frontWheel.x - backWheel.x) * 180 / PI + 90);
+	sf::Vector2f projected = moveTest.getPosition();
+	float tempColFront = frontWheel.x / 130;
+	float tempRowFront = frontWheel.y / 130;
+	float tempColBack = backWheel.x / 130;
+	float tempRowBack = backWheel.y / 130;
 
-	location = (frontWheel + backWheel) / 2.f;
-	heading = atan2(frontWheel.y - backWheel.y, frontWheel.x - backWheel.x);
+	sf::Vector2u collision = model->collidingWithBuilding(moveTest.getGlobalBounds());
+	if (collision.x == 100) {
+		if (tempColFront >= 0.01 && tempColFront <= model->mapWidth - 0.01 && tempColBack >= 0.01 && tempColBack <= model->mapWidth - 0.01 && tempRowFront >= 0.01 && tempRowFront <= model->mapHeight - 0.01 && tempRowBack >= 0.01 && tempRowBack <= model->mapHeight - 0.01)
+		{
+			location = (frontWheel + backWheel) / 2.f;
+			heading = atan2(frontWheel.y - backWheel.y, frontWheel.x - backWheel.x);
+			moveTest.setRotation(heading * 180 / PI + 90);
+		}
+		else {
+			moveTest.setPosition(location);
+			moveTest.setRotation(heading * 180 / PI + 90);
+		}
+	}
+	else {
+		moveTest.setPosition(location);
+		moveTest.setRotation(heading * 180 / PI + 90);
+	}
 
 	col = (int)location.x / 130;
 	row = (int)location.y / 130;
